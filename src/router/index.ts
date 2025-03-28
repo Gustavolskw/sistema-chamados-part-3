51f8e376203a327757d44c2464347724024ad2e8
Gustavolskw <gustavolschmidt13@gmail.com>
2025-03-27 21:43:35 -0300

    gerenciar-pedidos

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: '/pedidos/novo'
+      redirect: '/pedidos/gerenciar'
     },
     {
       path: '/pedidos/novo',
